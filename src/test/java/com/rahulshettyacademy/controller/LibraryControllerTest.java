3d13a0cc221227f4df2e25cdcbec9f4342711629
omidashouri <omidashouri@gmail.com>
2021-08-22 15:18:37 +0430

    9-1 finished : omidashouri : 00.05.31

--- src/test/java/com/rahulshettyacademy/controller/LibraryControllerTest.java
+++ src/test/java/com/rahulshettyacademy/controller/LibraryControllerTest.java
@@ -1,20 +1,26 @@
 package com.rahulshettyacademy.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rahulshettyacademy.repository.LibraryRepository;
 import com.rahulshettyacademy.service.LibraryService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 class LibraryControllerTest {
 
@@ -27,6 +33,8 @@ class LibraryControllerTest {
     @MockBean
     LibraryRepository libraryRepository;
 
+    @Autowired
+    MockMvc mockMvc;
 
 
     @Test
@@ -41,6 +49,23 @@ class LibraryControllerTest {
         AddResponse addResponse = (AddResponse) responseEntity.getBody();
         Assertions.assertEquals("1",addResponse.getId());
         Assertions.assertEquals("Success Book is Added",addResponse.getMsg());
+    }
+
+    @Test
+    void addBookImplementationWithMock() throws Exception {
+        Library library = this.buildLibrary();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonObject = objectMapper.writeValueAsString(library);
+
+
+        when(libraryService.buildId(anyString(),anyInt())).thenReturn("1");
+        when(libraryService.checkBookAlreadyExist("1")).thenReturn(false);
+        when(libraryRepository.save(any())).thenReturn(library);
+        this.mockMvc.perform(post("/addBook")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonObject))
+                    .andExpect(status().isCreated());
     }
 
     @Disabled
